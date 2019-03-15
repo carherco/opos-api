a4279d6f30e1260f1d3820db1a10722b04564b18
Carlos Herrera Conejero <carherco@gmail.com>
2019-03-15 12:28:33 +0100

    retoques en expresiones regulares

--- src/Service/ImportTestService.php
+++ src/Service/ImportTestService.php
@@ -6,11 +6,28 @@ class ImportTestService {
 
   public function txt2csv($plain_text) {
 
+    $plain_text_cleaned = $this->fixText($plain_text);
+//dump($plain_text_cleaned);
     $patrones = [
-      '/(\.\R\d+)\s/', // Números de pregunta sin estar seguidos de un punto
+      '/(\R\d+\..*)\R([Aa][\.\)].*)\R([Bb][\.\)].*)\R([Cc][\.\)].*)\R([Dd][\.\)].*)/', //Número de pregunta+.+pregunta+salto de línea+a)+respuesta+salto de línea+b)+respuesta+salto de línea+c)+respuesta+d)+respuesta
+      '/(\R\d+\..*)\R([Aa][\.\)].*)\R([Bb][\.\)].*)\R([Cc][\.\)].*)/', //Número de pregunta+.+pregunta+salto de línea+a)+respuesta+salto de línea+b)+respuesta+salto de línea+c)+respuesta
+    ];
+
+    $sustituciones = [
+      "$1;$2;$3;$4;$5",
+      "$1;$2;$3;$4"
+    ];
+    $resultado = preg_replace($patrones, $sustituciones, PHP_EOL.$plain_text_cleaned);
+
+    return $resultado;
+  }
+
+  public function fixText($text) {
+    $patrones = [
+      '/(\R\d+)\s/', // Números de pregunta sin estar seguidos de un punto
       '/\R\d+[\R|\s]/',  // Números de página
-      '/\b(\R\d+\.\s)/', // Signos . olvidados (seguidos de número de pregunta o seguidos de a), b) o c)
-      '/\b(\R[a-dA-D]\)\s)/', // Signos . olvidados (seguidos de número de pregunta o seguidos de a), b) o c)
+      '/\b(\R[a-dA-D][\)\.]\s)/', // Signos . olvidados seguidos de letra de pregunta
+      '/\b(\R\d+\.\s)/', // Signos . olvidados seguidos de número de pregunta
       '/\b\R/', // Saltos de línea no precedidos por signo de puntuación (:.?)
       '/,\R/', // Saltos de línea precedidos de una coma
       '/\)\R/', // Saltos de línea precedidos de un paréntesis de cierre
@@ -29,19 +46,6 @@ class ImportTestService {
       '$1'
     ];
 
-    $plain_text_cleaned = preg_replace($patrones, $sustituciones, $plain_text);
-dump($plain_text_cleaned);
-    $patrones = [
-      '/(\R\d+\..*)\R([Aa]\..*)\R([Bb]\..*)\R([Cc]\..*)\R([Dd]\..*)/', //Número de pregunta+.+pregunta+salto de línea+a)+respuesta+salto de línea+b)+respuesta+salto de línea+c)+respuesta+d)+respuesta
-      '/(\R\d+\..*)\R([Aa]\..*)\R([Bb]\..*)\R([Cc]\..*)/', //Número de pregunta+.+pregunta+salto de línea+a)+respuesta+salto de línea+b)+respuesta+salto de línea+c)+respuesta
-    ];
-
-    $sustituciones = [
-      "$1;$2;$3;$4;$5",
-      "$1;$2;$3;$4"
-    ];
-    $resultado = preg_replace($patrones, $sustituciones, PHP_EOL.$plain_text_cleaned);
-
-    return $resultado;
+    return preg_replace($patrones, $sustituciones, $text);
   }
 }
